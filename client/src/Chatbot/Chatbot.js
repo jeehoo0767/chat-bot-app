502bc2392838e0ad59822e9359b2b30de63c13c4
jeehoo0767 <jeehoo0767@naver.com>
2020-12-17 14:08:13 +0900

    Create textQuery function

--- client/src/Chatbot/Chatbot.js
+++ client/src/Chatbot/Chatbot.js
@@ -1,6 +1,45 @@
 import React from 'react'
+import Axios from 'axios'
 
 function Chatbot() {
+
+    const textQuery = async (text) => {
+        // 우리가 보낸 메세지를 관리 한다.
+        let conversation = {
+            who : 'user',
+            content : { // 우리가 보낸 text
+                text : {
+                    text : text
+                }
+            }
+        }
+
+        const textQueryVariables ={
+            text
+        }
+
+        // 챗봇이 보낸 메세지를 관리.
+        try {
+            const response = await Axios.post('/api/dialog/textQuery', textQueryVariables)
+            const content = response.data.fulfillmentMessages[0]
+
+            conversation = {
+                who : 'bot',
+                content : content
+            }
+            console.log(conversation)
+        } catch(error) {
+            conversation = {
+                who : 'user',
+                content : { // 우리가 보낸 text
+                    text : {
+                        text : " Error just occured, please check the problem"
+                    }
+                }
+            }
+            console.log(conversation)
+        }
+    }
 
     const keyPressHandler = (e) => {
         if(e.key === "Enter") {
@@ -10,7 +49,7 @@ function Chatbot() {
 
             // textquery 라우트에 요청을 보내야 한다
 
-            // textQuery(e.target.value);
+            textQuery(e.target.value);
 
             e.target.value = "";
         }
